331a66f66bc786d4652d1462f2e6d64252cdf1b8
linzijun <linzjk@yonyou.com>
2021-03-18 14:45:37 +0800

    笔记注释整理

--- cube-ui-project/src/router/hooks.js
+++ cube-ui-project/src/router/hooks.js
@@ -1,6 +1,6 @@
 import store from '@/store/store';
 import * as types from '@/store/actions-type';
-import auth from './auth';
+import authRoutes from './auth';
 
 // 跟路由相关的钩子
 
@@ -10,52 +10,45 @@ export default {
     next();
   },
   permission(to, from, next) {
-    // 如果没登录，访问课程页面，跳转至登录页
-    // 在页面切换的时候 需要拿到当前状态是否登录
-
-    if (!store.state.hasPermission) { // 没登录（或者刷新的时候）
-      // 总是判断token是否失效
-      store.dispatch(types.VALIDATE).then(() => {
-        // token有效，登录了，放行
-        if (to.name === 'login') { // 登录了就不要再访问登录页了
+    // hasPermission(vuex) 默认为 false
+    if (!store.state.hasPermission) { // 未登录或手动刷新页面
+      // 调用接口验证本地token
+      store.dispatch(types.VALIDATE).then(() => { // 本地 token 有效
+        if (to.name === 'login') { // 已登录访问登录页需要跳转至个人中心
           next('/profile');
         } else {
           next();
         }
-      }).catch(() => { // 失效
-        const needLogin = to.matched.some(route => route.meta.needLogin);
-        if (needLogin) { // 失效未登录就跳去登录页
+      }).catch(() => { // 本地 token 失效
+        const needLogin = to.matched.some(route => route.meta.needLogin); // 匹配路由及子路由
+        if (needLogin) { // 需要登录才能访问
           next('/login');
-        } else { // 其他情况放行
+        } else { // 不需要登录也可访问
           next();
         }
       });
-    } else { // 登录了
-      if (to.name === 'login') { // 登录了就不要访问登录页了（路由方式访问）
-        next(from);
+    } else { // 已登录
+      if (to.name === 'login') { // 已登录访问登录页需要跳转至个人中心
+        next('/profile');
       } else {
-        console.log('登录了判断');
         next();
       }
     }
   },
-  profileAuth(to, from, next) {
-    console.log('to.name', to, to.name, store.state.hasPermission);
-    // 因为上一个登录钩子执行了next，才走下一个钩子，所以这里即使刷新也是true
-    if (store.state.hasPermission) {
-      if (!store.state.menuPermission) { // 没设置过菜单权限
-        const authList = store.state.user.menuList.map(i => i.auth);
-        const newRoutes = auth.filter(route => {
-          return authList.includes(route.name);
-        });
-        this.addRoutes(newRoutes);
-        store.commit(types.SET_MENU_LIST);
-        console.log('不存在菜单的跳转');
-        next({...to, replace: true}); // 重新走一遍，这样在动态路由页面刷新也可以访问到
+  menuPermission(to, from, next) {
+    if (store.state.hasPermission) { // 用户需要处于登录状态
+      if (!store.state.menuPermission) { // 用户没设置过菜单权限
+        // 权限列表
+        const authList = store.state.user.menuList.map(item => item.auth);
+        // 动态路由
+        const newRoutes = authRoutes.filter(route => authList.includes(route.name));
+        this.addRoutes(newRoutes); // 注册动态路由
+        store.commit(types.SET_MENU_LIST); // 将 menuPermission 置为 true
+        next({ ...to, replace: true }); // hack 方法，解决在动态路由页面刷新时匹配到 404 页面的问题
+        // 因为是刷新，所以 replace 为 true，只保存一次当前页的历史记录
         return;
       }
     }
-    console.log('存在菜单的跳转');
     next();
   },
 };
